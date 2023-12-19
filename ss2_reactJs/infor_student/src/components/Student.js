1ccc21dd00036e00e8b8c37206978655e71ed7d3
minhthuan196 <minhthuandtu@gmail.com>
2023-12-19 14:29:49 +0700

    update ss2

--- ss2_reactJs/infor_student/src/components/Student.js
+++ ss2_reactJs/infor_student/src/components/Student.js
@@ -1,32 +1,37 @@
 import './student.css';
 
 const students = [
-    {
+    {   id:1,
         company: 'Alfreds Futterkiste',
         contact: 'Maria Anders',
         country: 'Germany'
     },
     {
+        id:2,
         company: 'Centro comercial Moctezuma',
         contact: 'Francisco Chang',
         country: 'Mexico'
     },
     {
+        id:3,
         company: 'Ernst Handel',
         contact: 'Roland Mendel',
         country: 'Austria'
     },
     {
+        id:4,
         company: 'Island Trading',
         contact: 'Helen Bennett',
         country: 'UK'
     },
     {
+        id:5,
         company: 'Laughing Bacchus Winecellars',
         contact: 'Yoshi Tannamuri',
         country: 'Canada'
     },
     {
+        id:6,
         company: 'Magazzini Alimentari Riuniti',
         contact: 'Giovanni Rovelli',
         country: 'Italy'
@@ -45,8 +50,8 @@ function Student() {
                 </tr>
                 </thead>
                 <tbody>
-                {students.map((student, index) => (
-                    <tr key={index}>
+                {students.map(student => (
+                    <tr key={student.id}>
                         <td>{student.company}</td>
                         <td>{student.contact}</td>
                         <td>{student.country}</td>

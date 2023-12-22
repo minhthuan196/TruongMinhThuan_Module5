d4742c1a949ef1c70f2ba44e9d8d97e8d4c322c4
minhthuan196 <minhthuandtu@gmail.com>
2023-12-22 18:45:30 +0700

    ss6

---
+++ ss6-nextJs/covid_19/src/app/covid/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function Covid() {
+    const [covid, setCovid] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/covids')
+            .then((response) => {
+                console.log(response);
+                setCovid(response.data);
+            })
+    }, []);
+    if (!covid) {
+        return null;
+    }
+    return (
+        <>
+            <table>
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Confirmed</th>
+                    <th>Active</th>
+                    <th>Recovered</th>
+                    <th>Recovered</th>
+                </tr>
+                </thead>
+                <tbody>
+                {covid.map(data => (
+                    <tr key={data.id}>
+                        <td>{data.Date}</td>
+                        <td>{data.Confirmed}</td>
+                        <td>{data.Deaths}</td>
+                        <td>{data.Recovered}</td>
+                        <td>{data.Active}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Covid;
